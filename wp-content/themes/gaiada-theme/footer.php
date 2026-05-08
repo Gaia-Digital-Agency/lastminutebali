acdcd4c9421da62d1beaaa7ff0aceae3a954e358
rizalchris <rizalchrisdev@gmail.com>
2026-05-08 16:36:44 +0800

    style: adjust banner gradient overlay, refine footer spacing, and remove unused social media links

--- wp-content/themes/gaiada-theme/footer.php
+++ wp-content/themes/gaiada-theme/footer.php
@@ -12,7 +12,7 @@ if ( ! $whatsapp_number ) {
 
 $whatsapp_digits = $whatsapp_number ? preg_replace( '/\D+/', '', $whatsapp_number ) : '';
 $whatsapp_message = "Hi, I'm interested in booking a room. Could you send over your rates and let me know what's available? Thanks!";
-$whatsapp_url     = $whatsapp_digits
+$whatsapp_url = $whatsapp_digits
 	? 'https://wa.me/' . $whatsapp_digits . '?text=' . rawurlencode( $whatsapp_message )
 	: '';
 ?>
@@ -27,31 +27,14 @@ $whatsapp_url     = $whatsapp_digits
 	</a>
 <?php endif; ?>
 <footer>
-	<div class="container pt-5">
+	<div class="container">
 		<!-- Bottom -->
-		<div class="d-flex flex-column flex-sm-row align-items-center py-5 border-top">
+		<div class="d-flex flex-column flex-sm-row align-items-center py-5">
 			<p class="mb-0">
 				©
 				<?php echo date( 'Y' ); ?>
 				<?php bloginfo( 'name' ); ?>. All rights reserved.
 			</p>
-
-			<ul class="list-unstyled d-flex">
-				<li class="ms-3">
-					<a class="link-body-emphasis" href="#" aria-label="Instagram">
-						<svg class="bi" width="24" height="24">
-							<use xlink:href="#instagram"></use>
-						</svg>
-					</a>
-				</li>
-				<li class="ms-3">
-					<a class="link-body-emphasis" href="#" aria-label="Facebook">
-						<svg class="bi" width="24" height="24">
-							<use xlink:href="#facebook"></use>
-						</svg>
-					</a>
-				</li>
-			</ul>
 		</div>
 
 	</div>
